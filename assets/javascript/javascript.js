998556c664124ca099c97e1bb6e2629ebc9d0617
edwardfrary <edwardfrary@gmail.com>
2021-05-18 12:09:54 -0400

    Update javascript.js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,3 +1,6 @@
+var cityNameAPI = "";
+var country = "";
+
 //Event handler for clicking the search button
 $("#searchbtn").on("click", function () {
     apiSearch();
@@ -11,11 +14,15 @@ function apiSearch() {
         .then(function (response) {
             //check if there is a response then json the data and send it to be processed
             if (response.ok) {
-                //save the city name entered into the text input box
+                //save the city name entered into the text input box and then fetch the co-ordinates of the city
                 saveCity(searchBox);
 
                 response.json().then(function (data) {
-                    renderData(data);
+                    //extract the city name and country from this first API
+                    cityNameAPI = data.name;
+                    country = data.sys.country;
+                    //push the coords into a new function to get a more complete API
+                    coordFetch(data.coord.lon, data.coord.lat);
                 });
             }
 
@@ -29,12 +36,36 @@ function apiSearch() {
 //function to handle clicking a button of a previous search to fetch an api
 function apiBtn(savedCity) {
     //fetch the API with the saved city name
-    fetch("http://api.openweathermap.org/data/2.5/weather?q=" + savedCity + "&appid=2fd9f6120b1e5e49f6b0893e50ef57f6")
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=" + savedCity + "&units=imperial&appid=2fd9f6120b1e5e49f6b0893e50ef57f6")
         .then(function (response) {
             //check if there is a response then json the data and send it to be processed
             if (response.ok) {
 
                 response.json().then(function (data) {
+                    //extract the city name and country from this first API
+                    cityNameAPI = data.name;
+                    country = data.sys.country;
+                    //push the coords into a new function to get a more complete API
+                    coordFetch(data.coord.lon, data.coord.lat);
+                });
+            }
+
+            //display error message if no response
+            else {
+                alert("INVALID ENTRY");
+            }
+        })
+};
+
+//function to get a more complete weather API based on corordinates of searched city
+function coordFetch(long, lati) {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lati + "&lon=" + long + "&units=imperial&appid=2fd9f6120b1e5e49f6b0893e50ef57f6")
+        .then(function (response) {
+            //check if there is a response then json the data to be sent for processing
+            if (response.ok) {
+
+                response.json().then(function (data) {
+                    //send the data to be rendered in HTML
                     renderData(data);
                 });
             }
@@ -61,7 +92,31 @@ function renderData(data) {
     console.log(data);
     //first clear the previous elements from any other searches
     $("#cityname-header").empty();
+    $("span").empty();
+
+    //now insert the city name and country
     var cityName = $("<h2>")
-        .text("City of: " + data.name + ", " + data.sys.country);
+        .text("City of: " + cityNameAPI + ", " + country);
     $("#cityname-header").append(cityName);
+
+    //TEMPERATURE
+    var temperature = $("<span>")
+        .text("Current temp: " + data.current.temp +"°F");
+    $("#temperature").append(temperature);
+
+    //WINDSPEED
+    var windSpeed = $("<span>")
+        .text("Wind Speed: " + data.current.wind_speed + " MPH");
+    $("#wind-speed").append(windSpeed);
+
+    //HUMIDITY
+    var humidity = $("<span>")
+        .text("Humidity: " + data.current.humidity + "%");
+    $("#humidity").append(humidity);
+
+    //UV INDEX
+    var uvIndex = $("<span>")
+        .text("UV Index: " + data.current.uvi);
+    $("#uvindex").append(uvIndex);
+
 };
