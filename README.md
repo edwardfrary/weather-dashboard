0aa422efed568b8dde24d899ead1d23c4dc9315b
edwardfrary <edwardfrary@gmail.com>
2021-05-19 11:35:39 -0400

    final update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # weather-dashboard
  
+When the user enters the name of a city into the search field and clicks the search button an api is fetched based on that city's name. The longtitude and latitude information from this api is then used to fetch a different API that has the weather information as well as a forcast of the weather of the coming week. This data is then used to display the current weather in the searched city as well as a 5 day forcast. The city's name is then saved in local storage as well as displayed on the webpage as a button that can be used to initiate another search of that city's weather forecast. 
+
+In addition there is a clear history button that removes all the stored names in local storage and subsequently refreshes the page. Thank you.
--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,6 @@
 var cityNameAPI = "";
 var country = "";
+var cityNameArr = [];
 
 //Event handler for clicking the search button
 $("#searchbtn").on("click", function () {
@@ -15,7 +16,7 @@ function apiSearch() {
             //check if there is a response then json the data and send it to be processed
             if (response.ok) {
                 //save the city name entered into the text input box and then fetch the co-ordinates of the city
-                saveCity(searchBox);
+                saveData(searchBox);
 
                 response.json().then(function (data) {
                     //extract the city name and country from this first API
@@ -77,7 +78,7 @@ function coordFetch(long, lati) {
         })
 };
 
-//function to save the city name as a clickable button and display it on index.html
+//function display the searched city name as a clickable button
 function saveCity(cityName) {
     var savedBtnFunction = "apiBtn('" + cityName + "');";
     var savedCityEl = $("<button>")
@@ -85,6 +86,43 @@ function saveCity(cityName) {
         .text(cityName)
         .attr("onClick", savedBtnFunction);
     $("#previous-searches").append(savedCityEl);
+
+};
+
+//retrieve, update then save the new data in an array. Sends the held/updated data to the saveCity function to render as buttons
+function saveData(cityName) {
+
+    //clear the previously generated buttons
+    $("#previous-searches").empty();
+
+    cityNameArr = JSON.parse(localStorage.getItem("city_name"));
+    cityNameArr.push(cityName);
+    localStorage.setItem("city_name", JSON.stringify(cityNameArr));
+    for (i = 0; i < cityNameArr.length; i++) {
+        saveCity(cityNameArr[i]);
+    }
+
+};
+
+//load data and render previous city buttons on first load of webpage
+function loadData() {
+
+    cityNameArr = JSON.parse(localStorage.getItem("city_name"));
+
+    if (!cityNameArr) {
+        cityNameArr = [];
+    };
+
+    localStorage.setItem("city_name", JSON.stringify(cityNameArr));
+    for (i = 0; i < cityNameArr.length; i++) {
+        saveCity(cityNameArr[i]);
+    }
+};
+
+//clear search history
+function clearHistory() {
+    localStorage.clear();
+    location.reload();
 };
 
 //gets the current date and add days to the current date if needed before returning the value
@@ -104,15 +142,19 @@ function dateHandler(days) {
 
 //function to render the current and predicted weather data to index.html
 function renderData(data) {
-
+    console.log(data);
     //first clear the previous elements from any other searches
     $("#cityname-header").empty();
     $("span").empty();
 
-    //insert the city name and country
+    //insert the city name and country as well as an icon depicting the weather conditions
     var cityName = $("<h2>")
         .text("City of: " + cityNameAPI + ", " + country + " (" + dateHandler(0) + ")");
-    $("#cityname-header").append(cityName);
+    var currentWeatherIcon = data.current.weather;
+    currentWeatherIconEl = $("<img>")
+        .attr("src", "http://openweathermap.org/img/wn/" + currentWeatherIcon[0].icon + "@2x.png");
+    $("#cityname-header").append(cityName).append(currentWeatherIconEl);
+
 
     //TEMPERATURE
     var temperature = $("<span>")
@@ -129,15 +171,49 @@ function renderData(data) {
         .text("Humidity: " + data.current.humidity + "%");
     $("#humidity").append(humidity);
 
-    //UV INDEX
-    var uvIndex = $("<span>")
-        .text("UV Index: " + data.current.uvi);
-    $("#uvindex").append(uvIndex);
+    //UV INDEX WITH DIFFERENT TEXT COLORS BASED ON SEVERITY
+    if (data.current.uvi <= 2) {
+        var uvIndex = $("<span>")
+            .addClass("text-success bg-dark")
+            .text("UV Index: " + data.current.uvi);
+        $("#uvindex").append(uvIndex);
+    }
 
+    else if (data.current.uvi <= 5 && data.current.uvi > 2) {
+        var uvIndex = $("<span>")
+            .addClass("text-warning bg-dark")
+            .text("UV Index: " + data.current.uvi);
+        $("#uvindex").append(uvIndex);
+    }
+
+    else if (data.current.uvi <= 7 && data.current.uvi > 5) {
+        var uvIndex = $("<span>")
+            .addClass("text-orange bg-dark")
+            .text("UV Index: " + data.current.uvi);
+        $("#uvindex").append(uvIndex);
+    }
+
+    else if (data.current.uvi <= 10 && data.current.uvi > 7) {
+        var uvIndex = $("<span>")
+            .addClass("text-danger bg-dark")
+            .text("UV Index: " + data.current.uvi);
+        $("#uvindex").append(uvIndex);
+    }
+
+    else {
+        var uvIndex = $("<span>")
+            .addClass("text-violet bg-dark")
+            .text("UV Index: " + data.current.uvi);
+        $("#uvindex").append(uvIndex);
+    };
     //---------FIVE DAY PREDICTION STARTS HERE--------
 
     for (var i = 1; i < 6; i++) {
-        //header for the prediction cards
+
+        //clear the previous prediction cards
+        $("#day" + [i]).empty();
+
+        //creation of new prediction card elements
         var dayCardHolder = "#day" + [i];
         var cardHeader = $(dayCardHolder);
 
@@ -147,11 +223,11 @@ function renderData(data) {
             .text(daysAhead);
 
         //body for the prediction cards
-        
+
         //ICON
         var weatherIconPNG = data.daily[i].weather;
         var weatherIconEl = $("<img>")
-        .attr("src", "http://openweathermap.org/img/wn/" + weatherIconPNG[0].icon + "@2x.png");
+            .attr("src", "http://openweathermap.org/img/wn/" + weatherIconPNG[0].icon + "@2x.png");
 
         //TEMPERATURE
         var tempCardBodyEl = $("<div>")
@@ -177,3 +253,5 @@ function renderData(data) {
     }
 
 };
+
+loadData();
